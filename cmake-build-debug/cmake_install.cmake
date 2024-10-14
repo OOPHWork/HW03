fba749e954d65988eef20849a2642b66da35eecd
HwangHyeongjin <80672495+HwangHyeongjin@users.noreply.github.com>
2024-10-15 08:57:22 +0900

    수정

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/hwanghyeongjin/CLionProjects/HW03-1
+# Install script for directory: /Users/hwanghyeongjin/Documents/GitHub/HW03-1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/hwanghyeongjin/CLionProjects/HW03-1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/hwanghyeongjin/Documents/GitHub/HW03-1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
